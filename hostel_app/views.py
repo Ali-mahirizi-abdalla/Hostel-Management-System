ed8497491e3d5c32978cc76caa405b7cdb71cbca
Ali-mahirizi-abdalla <alimahrez744@gmail.com>
2025-12-01 14:33:45 +0300

    Fix: Add render import to views.py

--- hostel_app/views.py
+++ hostel_app/views.py
@@ -1,6 +1,8 @@
-﻿from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 def index(request):
     return HttpResponse(' Hostel Management System - WORKING!<br><br>'
                        'Database setup may be needed.<br>'
                        'Run: python manage.py migrate')
+
