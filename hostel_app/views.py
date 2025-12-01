87f202740bfba7c0fdc0d68eba8a41222607f666
Ali-mahirizi-abdalla <alimahrez744@gmail.com>
2025-12-01 14:32:52 +0300

    Temp: Simplify index view for debugging

--- hostel_app/views.py
+++ hostel_app/views.py
@@ -1,30 +1,6 @@
-﻿def index(request):
-    'Display hostel management dashboard'
-    try:
-        rooms = Room.objects.all()
-        bookings = Booking.objects.all()
-        
-        total_rooms = rooms.count()
-        occupied_rooms = rooms.filter(status='occupied').count()
-        available_rooms = rooms.filter(status='available').count()
-        total_guests = bookings.filter(status__in=['checked_in', 'confirmed']).count()
-        
-    except Exception as e:
-        # Database not ready - show sample data
-        total_rooms = 20
-        occupied_rooms = 8
-        available_rooms = 12
-        total_guests = 15
-        rooms = []
-        bookings = []
-    
-    context = {
-        'rooms': rooms,
-        'bookings': bookings,
-        'total_rooms': total_rooms,
-        'occupied_rooms': occupied_rooms,
-        'available_rooms': available_rooms,
-        'total_guests': total_guests,
-    }
-    
-    return render(request, 'index.html', context)
+﻿from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse(' Hostel Management System - WORKING!<br><br>'
+                       'Database setup may be needed.<br>'
+                       'Run: python manage.py migrate')
